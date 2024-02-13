6427366a91debee7e16a0842eda2878912c909e3
ajitali <ajitabi123@gmail.com>
2024-02-13 15:38:17 +0530

    assignment1

---
+++ Ajith_sql/db1.sql
@@ -0,0 +1,17 @@
+-- create database ajith;
+-- use ajith;
+-- create table student(id int, name varchar(50));
+-- insert into student values(2004,"Amith");
+-- select *from student;
+-- select *from student where rollno=2001;
+-- delete from student where rollno=2001;
+-- select *from student;
+-- desc student;
+-- create table mark(rollno int, mark int);
+-- alter table mark add constraint p primary key(rollno);
+-- desc mark;
+-- insert into mark values(2004,95);
+
+
+
+
