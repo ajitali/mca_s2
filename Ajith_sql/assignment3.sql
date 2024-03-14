61593c3e6fb9e7b823533e6b26c70f05b7efdf11
ajitali <ajitabi123@gmail.com>
2024-03-14 15:10:13 +0530

    balance assignments

--- Ajith_sql/assignment3.sql
+++ Ajith_sql/assignment3.sql
@@ -3,12 +3,14 @@
 -- inner join student on student.student_no=grade.student_no
 -- inner join section on grade.section_identifier=section.section_identifier 
 -- inner join course on section.course_no=course.course_no where student.name="smith";
+
 -- ....................question_4..............
 -- select student.name,grade.grade from student 
 -- inner join grade on student.student_no=grade.student_no
 -- inner join section on grade.section_identifier=section.section_identifier 
 -- inner join course on section.course_no=course.course_no 
 -- where course.course_name="Database" and section.semester="fall";
+
 -- ....................question_5......................
 -- select course.course_name from course where course.course_no in(
 --  select prerequisite.prerequisite_no from prerequisite 
@@ -16,7 +18,7 @@
 --  where course.course_no in(select course.course_no from course where course_name="Database"));
 
 -- .............question_6...................
- create view student_view as select name from student where major="CS";
+-- create view student_view as select name from student where major="CS";
  
 -- ....................question_7.................
 --  select course_name from course where course_no in(
@@ -37,12 +39,13 @@
 --  where student.class=4 and major="CS";
  
 -- ................question_10.............
+-- a.
 -- insert into student values("Johnson",25,1,"Math");
-
+-- b.
 -- set sql_safe_updates=0;
 -- update student set class=2 where name="Smith";
 -- set sql_safe_updates=1;
-
+-- c.
 -- insert into course values("Knowledge engineering","CS4390",3,"CS");
-
+-- d.
 -- delete from student where student_no=17; 
